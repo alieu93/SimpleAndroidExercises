6a247d2e73bcc18cd52a2a58825c6842bf6644f0
alieu93 <adam.lieu@hotmail.com>
2017-03-14 12:32:50 -0400

    Added CardView layout and added some initial code

--- SimpleCardView/app/src/main/java/com/example/adamlieu/myapplication/MainActivity.java
+++ SimpleCardView/app/src/main/java/com/example/adamlieu/myapplication/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.adamlieu.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<UpcomingReleases> releases;
+    static View.OnClickListener myOnClickListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
