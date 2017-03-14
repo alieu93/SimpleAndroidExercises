080cbb03bd505b702f97d7a9cb23e1e2b2afcfab
alieu93 <adam.lieu@hotmail.com>
2017-03-14 12:10:34 -0400

    Initial commit

---
+++ SimpleCardView/app/src/main/java/com/example/adamlieu/myapplication/UpcomingReleases.java
@@ -0,0 +1,30 @@
+package com.example.adamlieu.myapplication;
+
+/**
+ * Created by Adam Lieu on 3/14/2017.
+ */
+
+public class UpcomingReleases {
+    private String titleName;
+    private String releaseDate;
+    private String storeURL;
+    private String imgURL;
+
+    private boolean isWindows;
+    private boolean isMac;
+    private boolean isLinux;
+
+    public UpcomingReleases(String titleName, String releaseDate, String storeURL, String imgURL){
+        this.titleName = titleName;
+        this.releaseDate = releaseDate;
+        this.storeURL = storeURL;
+        this.imgURL = imgURL;
+    }
+
+    public String getTitleName() { return titleName; }
+
+    public String getStoreURL(){ return storeURL; }
+
+    public String toText(){ return titleName + "\n" + releaseDate; }
+
+}
